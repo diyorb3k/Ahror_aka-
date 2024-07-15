dcf91ef6fdcb5f3612fdee6f9d6d749e716021f7
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-07-15 10:22:50 +0500

    salom

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,53 @@
 import React from "react";
+import './About.css'
 
 function About() {
   let a = "hello";
-  return <div>About</div>;
+  return <div className="cards">
+    <div class="card1">
+    <h2>CARD1</h2>
+
+    
+     </div>
+
+     <div class="card1">
+    <h2>CARD2</h2>
+
+    
+     </div>   
+
+
+     <div class="card1">
+    <h2>CARD3</h2>
+
+    
+     </div>
+
+
+     <div class="card1">
+    <h2>CARD4</h2>
+
+    
+     </div>
+
+     <div class="card1">
+    <h2>CARD5</h2>
+
+    
+     </div>
+
+
+
+     <div class="card1">
+    <h2>CARD6</h2>
+
+    
+     </div>
+
+
+
+    
+  </div>;
 }
 
 export default About;
